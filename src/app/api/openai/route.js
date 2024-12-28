9b2403a432c532447757cdeee66fa5bda83b4efa
Suhas Bhairav <techsaadhana@gmail.com>
2024-12-28 15:42:09 +0100

    init project

---
+++ src/app/api/openai/route.js
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+const openai = new OpenAI({
+  apiKey: process.env.NEXT_PUBLIC_OPEN_AI_KEY,
+});
+
+export async function POST(req) {
+  const data = await req.json();
+  console.log("Data:", data);
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "developer",
+        content: [
+          {
+            type: "text",
+            text: "You are a customer support executive for a mobile network company called Metric Coders based in India and the US. You answer questions about different mobile network plans, data plans and roaming plans. Just make up different plans. Provide costs in both rupees and dollars.",
+          },
+        ],
+      },
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: data["chatMessage"],
+          },
+        ],
+      },
+    ],
+  });
+
+  console.log("response:", response.choices[0].message.content);
+  
+  return NextResponse.json({
+    message: response.choices[0].message.content
+  });
+}
